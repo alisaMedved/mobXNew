b772c1252711178d90fcb371558b024f7ca1365f
alisaMedv <alisa.medvedeva@dz.ru>
2020-05-31 20:27:51 +0300

    when autorun + общие рассуждения

--- src/index.js
+++ src/index.js
@@ -1,42 +1,44 @@
 import './index.css';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { observable, computed, configure, action, decorate, runInAction } from 'mobx';
+import { observable, configure, action, when, autorun } from 'mobx';
 import { observer } from 'mobx-react';
 configure({ enforceActions: 'observed' });
 
 class Store {
-    user: null;
-
-    getUser() {
-       fetch(`https://randomuser.me/api/`)
-           .then(res => res.json())
-           .then(json => {
-               if (json.results) {
-                   runInAction(() => {
-                       this.user = json.results[0]
-                   });
-               }
-           })
-    };
+    @observable count = 0;
+    @action increment() { this.count++ };
+    @action decrement() { this.count-- };
 }
 
-decorate(Store, {
-    user: observable,
-    // теряется контекст вызова action из-за содержания внутри него ассинхронщины
-    getUser: action.bound,
-});
-
 const appStore = new Store();
+// when и autorun - это ключевые слова для создания пользовательской реакции
+// when(условие, действие при true) - выполняется единоразово при выполненнии условия. И после этого перестает наблюдатся вовсе
+when(
+    () => appStore.count > 5,
+    () => { alert('Count value is more than 5')}
+);
+
+// autorun - выполняется при componentWillMount и также при каждом изменении зависимости
+// зависимость - это любая вычисляемая или наблюдаемая переменная что задействована в этом autorun
+// autorun - запускается после изменения значения и перед его дальнейшей перерисовкой
+autorun(() => {
+    alert(`Count value is: ${appStore.count}`);
+},
+    {
+        name: 'Custom autorun',
+        delay: 3000,
+    });
 
 @observer class App extends Component {
+    handleIncrement = () => {this.props.store.increment() };
+    handleDecrement = () => {this.props.store.decrement() };
     render() {
-        const { store } = this.props;
-
         return (
             <div>
-                <button onClick={store.getUser}>Get User</button>
-                <h1>{store.user ? store.user.login.username : 'Default name'}</h1>
+                <h1>{this.props.store.count}</h1>
+                <button onClick={this.handleDecrement}>-1</button>
+                <button onClick={this.handleIncrement}>+1</button>
             </div>
         )
     }
